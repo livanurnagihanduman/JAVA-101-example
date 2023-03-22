2e48dc23fb684f468ca6b9ad2e275faf299aaeeb
Livanur Nagihan Duman <128521365+livanurnagihanduman@users.noreply.github.com>
2023-03-22 16:55:44 +0300

    Add files via upload

--- JAVA 101  example/src/Main.java
+++ JAVA 101  example/src/Main.java
@@ -59,6 +59,36 @@ public class Main {
         double hipotenüs = Math.pow((number1*number1)+(number2*number2),0.5);
         System.out.print("Dik Üçgenin Hipotenüs Uzunluğu = " + hipotenüs);
 
+        // Taksimetre Hesaplayan Program
+
+        Scanner a = new Scanner(System.in);
+        System.out.print("\n");
+
+        double yol ;
+        System.out.print("Lütfen gidilen mesafeyi km cinsinden yazınız: ");
+        yol = a.nextDouble();
+         double hesapla = yol * 7.5;
+         System.out.print("Ödnecek tutar TL cinsinden: " + hesapla);
+
+
+         // Dairenin Çevresini ve Alanını Hesaplayan Program
+
+        Scanner b = new Scanner(System.in);
+        System.out.print("\n");
+
+        float sayi1;
+        float sayi2 = 3.14F;
+
+        System.out.print("Lütfen bir sayı giriniz: ");
+        sayi1 = b.nextFloat();
+
+        double cevre = 2*sayi2*sayi1;
+        System.out.print("Dairenin Çevresi: " + cevre);
+
+        System.out.print("\n");
+
+        double alan = sayi2*(sayi1*sayi1);
+        System.out.print("Dairenin Alanı: " + alan);
 
     }
 }
